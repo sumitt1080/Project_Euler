750fa8413f94aafb85015b0c54c68d7bfabcc41f
sumitt1080 <51052011+sumitt1080@users.noreply.github.com>
2019-10-18 00:41:45 +0530

    Add files via upload

---
+++ euler problem 9.java
@@ -0,0 +1,16 @@
+public class Program
+{
+    
+   
+    public static void main(String[] args) 
+    {
+      
+       
+       int i,j,c; 
+       for(i=1;i<=500;i++){ for(j=1;j<=500;j++)
+       { c=1000-i-j; 
+       if(i*i+j*j-c*c==0&&i<j)
+       { System.out.println("a * b * c = "+(i*j*c)); } }}
+        
+    }
+}
