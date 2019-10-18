5bc9055a6710b7feaac2d67a6a4a5169cb65ff79
sumitt1080 <51052011+sumitt1080@users.noreply.github.com>
2019-10-19 00:19:53 +0530

    Update euler problem 5.java

--- euler problem 5.java
+++ euler problem 5.java
@@ -1,21 +1,22 @@
-class Program
+public class Program
 {
     public static void main(String[] args) {
-        int c = 0;
-        for (int i = 2; i <= 1000000000; i+=2) {
-            boolean x = true;
-            for(int j  = 1;j<=20;j++){
-                x = true;
-                if(i%j != 0) {
-                    x = false;
-                    break;
-                }
-            }
-            if(x == true) {
-                c = i;
-                break;
+        System.out.println(findMult(20));
+    }
+    
+    public static boolean isMult(int num) {
+        for (int i = 11; i < 20; i++) {
+            if (num % i != 0) {
+                return false;
             }
         }
-        System.out.println(c);
+        return true;
+    }
+    
+    public static int findMult(int num) {
+        while (!isMult(num)) {
+            num++;
+        }
+        return num;
     }
 }
