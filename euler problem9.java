0f5ef00664f8c7f400de0af22f3da95c83de877a
sumitt1080 <51052011+sumitt1080@users.noreply.github.com>
2019-10-24 10:02:37 +0530

    Update and rename euler problem 9.java to euler problem9.java

--- euler problem9.java
+++ euler problem9.java
@@ -10,7 +10,8 @@ public class Program
        for(i=1;i<=500;i++){ for(j=1;j<=500;j++)
        { c=1000-i-j; 
        if(i*i+j*j-c*c==0&&i<j)
-       { System.out.println("a * b * c = "+(i*j*c)); } }}
+       { System.out.println("a * b * c = "+(i*j*c));
+System.exit(0); } }}
         
     }
 }
